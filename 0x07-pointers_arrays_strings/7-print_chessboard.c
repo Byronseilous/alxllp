58511f2ae138ee7f1bfbf7b3d4f03d615efae0dd
Byronseilous <104057957+Byronseilous@users.noreply.github.com>
2023-02-15 23:43:48 +0300

    chore: Update 7-print_chessboard.c (7-print_chessboard.c)

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - a function that prints the chessboard.
+ *
+ * @a: row of two dimensional array.
+ *
+ *
+ * Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+
+		_putchar('\n');
+
+	}
+}
+
