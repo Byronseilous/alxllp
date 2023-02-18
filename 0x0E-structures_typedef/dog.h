7fca702b719dcc0ef98ae923962db261dbae88d3
Byronseilous <104057957+Byronseilous@users.noreply.github.com>
2023-02-18 06:52:30 +0300

    feat: Modify C/C++ header file in (dog.h)

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+#define UNUSED(x) (void)(x)
+
+/**
+ * struct dog - contains details of a pet dog.
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
