f515c9addc4bf8cc105458f12a6b4a48eb65e613
Byronseilous <104057957+Byronseilous@users.noreply.github.com>
2023-02-28 08:24:37 +0300

    chore: Update print_square.c (print_square.c)

---
+++ 0x04-more_functions_nested_loops/print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'This program prints numbers, from 0 t0 9'
+ * The printout is followed by a newline
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r, c;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	{
+
+		for (r = 1; r <= n; r++)
+		{
+			for (c = 1; c <= n; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+	}
+
+	return (0);
+}
