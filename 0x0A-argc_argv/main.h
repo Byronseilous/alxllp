50d180120e901e6a29cb58e2624e7e0af9f6523e
Byronseilous <104057957+Byronseilous@users.noreply.github.com>
2023-02-28 08:46:13 +0300

    feat: Modify C/C++ header file in (main.h)

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define UNUSED(x) (void)(x)
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+
+#endif /* MAIN_H */
